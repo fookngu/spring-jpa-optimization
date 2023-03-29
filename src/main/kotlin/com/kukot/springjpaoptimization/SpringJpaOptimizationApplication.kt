e7e3ab6121b27e2d5b386b4e4d8b6544d2f96633
Kevin Nguyen <kevin.nguyen@contour.network>
2023-03-29 17:00:37 +0800

    initial commit with minimal dependencies

---
+++ src/main/kotlin/com/kukot/springjpaoptimization/SpringJpaOptimizationApplication.kt
@@ -0,0 +1,11 @@
+package com.kukot.springjpaoptimization
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJpaOptimizationApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringJpaOptimizationApplication>(*args)
+}
---
+++ src/test/kotlin/com/kukot/springjpaoptimization/SpringJpaOptimizationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kukot.springjpaoptimization
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringJpaOptimizationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
